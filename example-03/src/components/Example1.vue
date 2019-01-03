1a74d9ff51d49ee971a6f5d3b873850ce2ffcbaa
mapserver2007 <mapserver2007@gmail.com>
2019-01-03 16:32:01 +0900

    vue-routerを使うexample03追加

---
+++ example-03/src/components/Example1.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <router-link to="/example2">Example2</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "rotuer example1"
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ example-03/src/components/Example2.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <router-link to="/example1">Example1</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "rotuer example2"
+    }
+  }
+}
+</script>
+
+<style>
+</style>
